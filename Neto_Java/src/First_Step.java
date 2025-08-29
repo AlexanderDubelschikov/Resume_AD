a2933c787698481cefe48723149310a524f9ce4b
AlexanderDubelschikov <109314029+AlexanderDubelschikov@users.noreply.github.com>
2025-08-29 05:30:36 +0300

    FirstCommit

---
+++ Neto_Java/src/First_Step.java
@@ -0,0 +1,31 @@
+public class First_Step {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("2+3");
+        System.out.println(2+3);
+        System.out.println("Hello" + "World");
+
+        int x;
+        x = 10;
+        System.out.println(x);
+        int y = 15;
+        System.out.println(y);
+        x = 20;
+        System.out.println(x);
+
+        double d = 3.5;
+        System.out.println(d);
+
+        String name = "Alexander";
+        System.out.println("Hello " + name);
+
+        int _new = 5;
+        int _new2 = _new + 1;
+        _new = 10;
+        System.out.println(_new2);
+        System.out.println(_new);
+
+    }
+}
---
+++ Neto_Java/src/HW_1.java
@@ -0,0 +1,11 @@
+public class HW_1 {
+    public static void main(String[] args)
+    {
+        System.out.println("Alexander Dubelschikov");
+
+        int income = 50000;
+        int spending = 40000;
+        System.out.println("Итого (руб):");
+        System.out.println(income-spending);
+    }
+}
