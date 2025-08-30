610b2416c6b5c6c8a073a8a4ac7b98b6f19ee9f7
AlexanderDubelschikov <109314029+AlexanderDubelschikov@users.noreply.github.com>
2025-08-30 17:25:22 +0300

    Next

--- Neto_Java/src/Lesson_1.java
+++ Neto_Java/src/Lesson_1.java
@@ -1,11 +1,21 @@
 public class Lesson_1 {
 
+    public static String name;
     public static void printDelim(String title){ //Статический метод(ПАПАМЕТР)
         System.out.println();
         System.out.println();
         System.out.println("^^^^^^^^");
         System.out.println(title);
         System.out.println("________");
+    }
+
+    public static void printMult(int a, int b){
+        System.out.println(a*b);
+    }
+
+    public static void calcDep (int sum, int month, float perc){
+        System.out.println("Сумма вклада: " + sum + "; Месяцев до выплат: " + month + "; Процент в месяц: " +
+                perc + "; Сумма по возвращению вклада: " + (sum + month * (sum*perc)));
     }
     public static void main(String[] args){
         printDelim("ДЕМОГРАФИЯ");
@@ -16,6 +26,9 @@ public class Lesson_1 {
         System.out.println("Пенсия: 10000");
         printDelim("МЕДЕЦИНА");
         System.out.println("Группа крови: На рукаве");
+
+        printMult(10,10);
+        calcDep(100,1,0.1f);
 
     }
 }
