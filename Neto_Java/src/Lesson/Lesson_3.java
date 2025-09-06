ef88a5e24a028573fac2044996ff24b018157819
AlexanderDubelschikov <109314029+AlexanderDubelschikov@users.noreply.github.com>
2025-09-06 04:33:34 +0300

    Lesson 3 step

--- Neto_Java/src/Lesson/Lesson_3.java
+++ Neto_Java/src/Lesson/Lesson_3.java
@@ -4,9 +4,34 @@ package Lesson;
 
 public class Lesson_3 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception{
 
+        // byte - -128 до 127
+        // short - -32768 до 32767
+        // int - -2^31 до 2^31-1 (2 лярда)
+        // long - -2^63 до 2^63-1
+
+        byte b = 3;
+        short s = 15;
         int x = 5;
+        long q = 5_000_000_000L;
+
+        int i = 10;
+        long i2 = i;
+
+        long r = 10_000_000_000L;
+        int r2 = (int) r;
+        System.out.println(r2);
+
+        int sp = 1;
+        while (true){
+            System.out.println(sp);
+            sp *=10;
+            Thread.sleep(300);
+        }
+
+        //При сложении (других операциях) целых чисел с разными типами, итог будет самым "жирным" типом.
+
 
 
 
