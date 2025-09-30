289b9461ac02720731ef496f9f9df57374528f16
AlexanderDubelschikov <109314029+AlexanderDubelschikov@users.noreply.github.com>
2025-09-30 06:31:25 +0300

    Next_step_lesson

--- Neto_Java/src/Lesson/Lesson_6.java
+++ Neto_Java/src/Lesson/Lesson_6.java
@@ -1,6 +1,6 @@
 package Lesson;
 
-import java.lang.reflect.Array;
+import java.sql.Array;
 import java.util.Arrays;
 
 public class Lesson_6 {
@@ -15,21 +15,55 @@ public class Lesson_6 {
         System.out.println(arr[4]);
         System.out.println(Arrays.toString(arr));
 
-        int[] arr2 = {10,105,0,100,0};
+        int[] arr2 = {10, 105, 0, 100, 0};
         System.out.println(Arrays.toString(arr2));
 
-        System.out.println(Arrays.equals(arr,arr2));
+        System.out.println(Arrays.equals(arr, arr2));
 
         String[] arr3 = new String[5];
         arr3[0] = "Hello";
         arr3[4] = "World";
         System.out.println(Arrays.toString(arr3));
 
-        Singer[] arr4 = { new Singer(),
-            new Singer()
+        Singer[] arr4 = {new Singer(),
+                new Singer()
         };
         System.out.println(Arrays.toString(arr4));
 
+        System.out.println("================");
+
+        String[] names = {
+                "Petya",
+                "Olya",
+                "Pavel",
+                "Ann",
+                "Alex"
+        };
+
+        /*if (!names[0].startsWith("P")){
+            System.out.println(names[0]);
+        }
+        if (!names[1].startsWith("P")){
+            System.out.println(names[1]);
+        }*/
+
+        /*for (int i = 0; i < names.length; i++) {
+            if (!names[i].startsWith("P")) {
+                System.out.println(names[i]);
+            }
+
+        }*/
+
+        /*for (String name : names){
+            if (!name.startsWith("P")) {
+                System.out.println(name);
+            }
+        }*/
+
+        Arrays.sort(names);
+        System.out.println(Arrays.toString(names));
+
+        String[] names2 = names.clone();
 
     }
 }
---
+++ Neto_Java/src/Lesson/Lesson_6_part_2.java
@@ -0,0 +1,18 @@
+package Lesson;
+
+import java.util.Scanner;
+
+public class Lesson_6_part_2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            System.out.println("Введи два числа: дата и температура");
+            String input = scanner.nextLine();
+
+        }
+
+    }
+
+}
