6381fbeb2ac7e725320e98ec7c2371e30a09775a
AlexanderDubelschikov <109314029+AlexanderDubelschikov@users.noreply.github.com>
2025-10-09 18:30:34 +0300

    Lesson7_end

--- Neto_Java/src/Lesson/Lesson_7.java
+++ Neto_Java/src/Lesson/Lesson_7.java
@@ -1,7 +1,32 @@
 package Lesson;
 
-public class Lesson_7{
+import java.util.Arrays;
+
+public class Lesson_7 {
     public static void main(String[] args) {
         int[] arr = new int[5];
+        //int [][] arr2 = new int[3][5];
+        int[][] arr2 = {
+                new int[1],
+                new int[2],
+                {1, 22, 3}
+        };
+        int[][][] arr3 = new int[10][10][10];
+        //arr2[0] = new int[1];
+        //arr2[1] = new int[2];
+        //arr2[2] = new int[3];
+        arr2[1][1] = 15;
+
+        for (int i = 0; i<arr2.length; i++){
+            //int[] tmp = arr2[i];
+            //System.out.println(Arrays.toString(tmp));
+            for (int j = 0; j<arr2[i].length; j++){
+                System.out.print(arr2[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
+        //System.out.println(Arrays.deepToString(arr2));
     }
 }
